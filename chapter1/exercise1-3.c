cefc80be9b3d422f75e84f7bfe7ec681c4ca3369
Patrick Hsieh <patrick.tk.hsieh@gmail.com>
2026-08-06 12:14:26 +0800

    feat: solve exercise 1-3

---
+++ chapter1/exercise1-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      
+    upper = 300;
+    step = 20;
+
+    printf("Fahrenheit\tCelsius\n");
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%10.0f %12.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    return 0;
+}
