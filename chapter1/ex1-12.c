9bec4d3240c2a4ae866ac72f1371b0fd5af6b886
Patrick Hsieh <patrick.tk.hsieh@gmail.com>
2026-08-07 10:47:48 +0800

    feat: solve exercise 1-12

---
+++ chapter1/ex1-12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main(int argc, char *argv[]) {
+    int c, state;
+    state = OUT;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            state = OUT;
+        }
+        else if (state == OUT) {
+            state = IN;
+            putchar('\n');
+        }
+
+        putchar(c);
+    }
+
+    return 0;
+}
